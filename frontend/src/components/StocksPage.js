a8deec852faaf4dc966756350b2bc6061e5504e0
Anshika Maheshwari <83770244+anshi20@users.noreply.github.com>
2024-08-08 06:34:52 +0000

    Create StocksPage.js

---
+++ frontend/src/components/StocksPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function StocksPage() {
+  return (
+    <div>
+      <h1>Stocks Page</h1>
+      {/* Add content or components specific to the Assets page here */}
+    </div>
+  );
+}
+
+export default StocksPage;
