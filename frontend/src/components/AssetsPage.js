c3831618a8d0ee0e331fb76fd6d7ca5d9d73437d
Anshika Maheshwari <83770244+anshi20@users.noreply.github.com>
2024-08-08 06:33:00 +0000

    Create AssetsPage.js

---
+++ frontend/src/components/AssetsPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AssetsPage() {
+  return (
+    <div>
+      <h1>Assets Page</h1>
+      {/* Add content or components specific to the Assets page here */}
+    </div>
+  );
+}
+
+export default AssetsPage;
