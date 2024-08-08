3e0ef4f37d5082fbd4362ffa148d2532e289c59b
Anshika Maheshwari <83770244+anshi20@users.noreply.github.com>
2024-08-08 06:37:34 +0000

    Create TransactionsPage.js

---
+++ frontend/src/components/TransactionsPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TransactionsPage() {
+  return (
+    <div>
+      <h1>Transactions Page</h1>
+      {/* Add content or components specific to the Assets page here */}
+    </div>
+  );
+}
+
+export default TransactionsPage;
